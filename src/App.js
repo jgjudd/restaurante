ecfb179e55b9c0a5c56a14ad54ab634496982f97
Justin <jgjudd40@gmail.com>
2021-01-15 08:22:45 -0500

    Sort by State, ascending/descending order

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Table from './components/Table'
 import './App.css';
 
 const App = () => {
   const [results, setResults] = useState([])
-    
+
   useEffect(() => {
-      fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
+      async function getRestaurants() {
+        await fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
           headers: {
               Authorization: 'Api-Key q3MNxtfep8Gt',
           },
-      })
-      .then(response => response.json())
-      .then(json => setResults(json))
+        })
+        .then(response => response.json())      
+        .then(json => setResults(json))
+      }
+      getRestaurants()
   }, [])
 
   return (
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import { sortByName } from '../utilities/sortFunctions'
+import React, { useState } from 'react'
 
 const Table = ({ restaurants }) => {
-    let sortedRestaurants = restaurants.sort(sortByName)
+    const [sortObject, setSortObject] = useState({ sortField: 'name', direction: 'ascending' })
+    
+    const handleSort = (sortField) => {
+        let direction = 'ascending';
+        if (sortObject.sortField === sortField && sortObject.direction === 'ascending') {
+            direction = 'descending';
+        }
+        setSortObject({ sortField, direction });
+    }
+
+    const sortedRestaurants = [...restaurants].sort((a, b) => {
+        if (a[sortObject.sortField] < b[sortObject.sortField]) {
+            return sortObject.direction === 'ascending' ? -1 : 1
+        }
+        if (a[sortObject.sortField] > b[sortObject.sortField]) {
+            return sortObject.direction === 'ascending' ? 1 : -1
+        }
+        return 0
+    })
+
     return (
         <table>
             <thead>
                 <tr>
                     <td>Name</td>
                     <td>City</td>
-                    <td>State</td>
+                    <td><button type='button' onClick={() => handleSort('state')}>State</button></td>
                     <td>Telephone</td>
                     <td>Genre</td>
                 </tr>

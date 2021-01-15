ae22675eb3d85ce4df080a4b0d21917e5a9b1523
Justin <jgjudd40@gmail.com>
2021-01-15 14:12:19 -0500

    basic header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+    width: 100vw;
+    height: 10vh;
+    background-color: red;
+`
 
 const Header = () => {
-    return <header>Header Component</header>
+    return <StyledHeader>Header Component</StyledHeader>
 }
 
 export default Header

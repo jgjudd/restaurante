51c17ebc91b875f0b3d48d442c56400511dc94b1
Justin <jgjudd40@gmail.com>
2021-01-17 09:13:46 -0500

    SearchBar refactor

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import Button from './Button'
 import styled from 'styled-components'
 
 const SearchBarContainer = styled.div`
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    align-items: left;
+    min-width: 25%;
 `
 const StyledInput = styled.input`
     font-size: 2rem;
-    
-    display: flex;
-    flex-direction: row;
 
 `
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
     return (
         <SearchBarContainer>
-            <StyledInput type='text' placeholder='Filter By Name/City/Genre' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-            <Button onClick={() => setSearchTerm('')} text='Clear' color='red' backgroundColor='white' />
+            <labe>Filter Results By Name, City, or Genre</labe>
+            <StyledInput 
+                type='text' 
+                placeholder='Search Term...' 
+                value={searchTerm} 
+                onChange={(e) => setSearchTerm(e.target.value)} 
+            />
         </SearchBarContainer>
     )
 }
